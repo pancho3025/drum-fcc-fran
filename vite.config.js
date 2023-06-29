a532d08b14f1856be7908659d40b4ce0e86708f7
francisco espeche <99774117+pancho3025@users.noreply.github.com>
2023-06-29 10:34:39 -0300

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://pancho3025.github.io/drum-machine-fcc-fran'
+  base: 'https://pancho3025.github.io/drum-fcc-fran'
 })
